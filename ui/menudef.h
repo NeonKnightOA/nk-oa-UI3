899042bb530cc69fe68c38272a85942ec2b269f5
Neon_Knight <openarena.arg@gmail.com>
2018-12-10 20:41:59 -0300

    Finished the refactoring. Fixed A LOT of bugs.

--- ui/menudef.h
+++ ui/menudef.h
@@ -384,7 +384,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define VOICECHAT_PRAISE			"praise"				// you did something good
 
 // if building with this in your ../ui/ file you MUST remove the ui/!!
-#include "ui/lang_english.h" // load english language
+// #include "ui/defs/lang_english.h" // load english language
 // #include "lang_fr.h" // load french
 // #include "lang_de.h" // load german
 // #include "lang_sp.h" // load spanish
