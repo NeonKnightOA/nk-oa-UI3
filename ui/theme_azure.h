e53efe6f3eb135d254e7cd8536bc8b616d7436b8
Neon_Knight <openarena.arg@gmail.com>
2018-12-11 17:58:09 -0300

    Adding missing defines in menudef, and making some menudefs theme-specific.

--- ui/theme_azure.h
+++ ui/theme_azure.h
@@ -107,3 +107,4 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 #define MP_KEYBINDMENU		"ui/panels/keybind2.bit"
 // uncomment this for simpler keybind choices
 
+/* For general menu sorting */
